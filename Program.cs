5cabeaa56988d6d7b7c3fc525483e5320a2dcf6e
gmsandhu12 <40672174+gmsandhu12@users.noreply.github.com>
2018-11-13 09:41:27 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rover r = new Rover();
+            r.PosX = 10;
+            r.PosY = 10;
+            r.CurrentDirection = "N";
+            Console.SetCursorPosition(r.PosX, r.PosY);
+            Console.WriteLine("R");
+            Console.Read();
+        }
+    }
+}
---
+++ Rover.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Rover
+    {
+        public int PosX { get; set; }
+        public int PosY { get; set; }
+        public string CurrentDirection { get; set; }
+    }
+}
